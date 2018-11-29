e47d93889f6be1f527cbdebabdeb51349efb90e7
Adriaan Giel <a3aan_G@live.nl>
2018-11-29 15:18:38 +0100

    - Filled user seeder with needed data - Set the order to run in the DatabaseSeeder

---
+++ Greenify/database/seeds/UserTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(\App\User::class,20)
+            ->create()
+            ->each(function($user){
+                $profile = factory(\App\Profile::class)->create([
+                    'user_id' => $user->id
+                ]);
+            });
+    }
+}
