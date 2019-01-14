9d3a5b63144a6223d31f33c0c4d2f8ffc9d48103
ZsZJ <ktj.lim@gmail.com>
2019-01-14 22:38:31 +0100

    add button back and linkbutton

---
+++ Greenify/resources/js/components/layout/ButtonBack.vue
@@ -0,0 +1,16 @@
+<template>
+    <router-link :to="{name: name}" class="arrow">
+        <i class="fas fa-arrow-left"></i>
+    </router-link>
+</template>
+
+<script>
+export default {
+    props: ['name']
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
---
+++ Greenify/resources/js/components/layout/LinkButton.vue
@@ -0,0 +1,44 @@
+<template>
+    <div :class="`button ${props.color}`">
+        <router-link :to="props.link">
+            {{props.text}}
+        </router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    props:['props']
+}
+</script>
+
+<style lang="scss" scoped>
+    .button {
+        width: 40vw;
+        padding: .5em;
+        border-radius: 3px;
+        box-shadow: 0px 1px 5px #8888889d;
+        font-weight: bold;
+        text-transform: uppercase;
+        text-align: center;
+        &.primary {
+            background: #535C68;
+        }
+        &.green {
+            background: #BADC58;
+        }
+        &.blue {
+            background: #7ED6DF;
+        }
+        &.red {
+            background: #EB4D4B;
+        }
+        a {
+            color: #fff;
+            text-decoration: none;
+            letter-spacing: -1px;
+        }
+    }
+</style>
+
+
