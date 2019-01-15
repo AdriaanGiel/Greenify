443050a49cdefcf2952ed8f8522c70bfbbc08a45
Marleen <aan_marleen@hotmail.com>
2019-01-15 14:51:04 +0100

    company button erbij

---
+++ Greenify/resources/js/components/layout/company-button.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="button">
+        <img src="smiley.gif" alt="Smiley face">
+        <a :href='linkDetailCompany'>Company</a>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ButtonDetailCompany",
+        props: ['linkDetailCompany']
+    }
+</script>
+
+<style lang="scss" scoped>
+    img{
+        height: 16vw;
+        width: 16vw;
+        border-radius: 12vw;
+        background-color: red; 
+        float: left;
+    }
+    .button{
+        box-sizing: border-box;
+        margin: 5vw;
+        padding: 4vw 0vw 0vw 4vw!important;
+        width: 90vw;
+        height: 24vw;
+        background-color: white;
+        // border-radius: 3px;
+        border-color: red;
+        border-style: solid;
+        border-width: 1px;
+        &:active{
+            background-color: #3e8214;
+            box-shadow: 0px 0px #888888;
+        }
+    }
+    a{
+        margin: 3vw;
+        float: left;
+        text-decoration: none;
+        color: grey;
+        font-size: 8vw;
+        font-weight: bold;
+        text-transform: uppercase;
+        letter-spacing: -1px;
+    }
+</style>
