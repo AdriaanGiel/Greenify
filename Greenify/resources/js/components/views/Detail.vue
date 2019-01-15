591d09885aba1786b0ecc8fbb3657f9afa51bc3f
Marleen <aan_marleen@hotmail.com>
2019-01-15 19:19:37 +0100

    detail pagina begin

--- Greenify/resources/js/components/views/Detail.vue
+++ Greenify/resources/js/components/views/Detail.vue
@@ -1,14 +1,26 @@
 <template>
-    <page-wrapper color="blue">
+    <page-wrapper color="purple">
+<h1>Company</h1>
 
+    <button-to-app :LinkSite='link'>
+    </button-to-app>
 
-
+<p>Beschrijving van het product of het restaurant of de winkel</p>
 
     </page-wrapper>
 </template>
 
 <script>
+    import ButtonToApp from '../layout/ButtonToApp'
     export default {
+        components: {
+            ButtonToApp
+        },
+        data() {
+            return {
+                link: '#'
+            }
+        },
         name: "DetailPage"
     }
 </script>
